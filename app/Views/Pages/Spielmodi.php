8b7a8d95a421425314035543e633df08be176db6
ln-iubh <100613094+ln-iubh@users.noreply.github.com>
2022-04-23 17:15:42 +0200

    Update Menu.php

---
+++ app/Views/Pages/Spielmodi.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <link rel="stylesheet" href="css/style2.css">
+</head>
+
+
+<body>
+
+<h1>Home</h1>
+
+<script>
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+    }
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+    }
+</script>
+</body>
+</html>
