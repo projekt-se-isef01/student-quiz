e7bd503997bad9ab81f1a31567380007b9aac9f0
BR <ccc.reitner@gmx.de>
2022-04-26 15:49:51 +0200

    test

--- app/Views/Templates/Header.php
+++ app/Views/Templates/Header.php
@@ -1,6 +1,8 @@
 <html>
 <head>
+    <link rel="stylesheet" type="text/css" href="/css/style2.css">
     <link rel="stylesheet" type="text/css" href="/css/style.css">
+
     <script src="/js/menu.js"></script>
 
 <body>
